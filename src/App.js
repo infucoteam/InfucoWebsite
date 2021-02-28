5137826e2b7ba11069f26be75d948cd6d561f773
infucoteam <78647950+infucoteam@users.noreply.github.com>
2021-02-28 09:41:26 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Link, Redirect, Route, Switch } from "react-router-dom";
 import HomePage from './pages/Homepage/HomePage';
 import Membership from './pages/Membership';
 import ContactUs from './pages/ContactUs';
+import Form from './pages/Form';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/membership" component={Membership} />
-      <Route exact path="/contactus" component={ContactUs} />
+      <Route exact path="/contactus" component={Form} />
       <Redirect to='/'/>
   </Switch>
   );
